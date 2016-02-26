a694e7d83f7d8e0d338383ea4fd347f42fa421f0
Ubuntu <ubuntu@ip-172-31-21-70.eu-west-1.compute.internal>
2016-02-26 07:28:27 +0000

    changes done in server

--- app/scripts/controllers/loginCTRL.js
+++ app/scripts/controllers/loginCTRL.js
@@ -4,9 +4,17 @@
 routerApp
   .controller('loginCTRL', function($cookies, $rootScope,$scope, $http, $state) {
     $scope.user = [];
+	
+	  $scope.$on('LOAD', function() {
+        $scope.loading = true;
+      });
+      $scope.$on('UNLOAD', function() {
+        $scope.loading = false;
+      });
     
     var URL = 'http://fabfresh.elasticbeanstalk.com';
     $scope.submitForm = function() {
+	$scope.$emit('LOAD');
       $scope.user = {
         "username": $scope.login.email,
         "password": $scope.login.password
@@ -18,10 +26,11 @@ routerApp
       headers : {'Content-Type': 'application/json'} 
      })
       .success(function(data) {
+	$scope.$emit('UNLOAD');
         if (data.errors) {
           alert("Some error occured");
         }
-        else if(data.status=="Not Authenticated"){
+        else if(data.status=="Not Authenticated"){    
             alert("Either email or password is wrong");
         }
         else {
